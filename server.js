44f37a44683561e110314d69d626c25f714c30b2
MichAle00 <124629021+MichAle00@users.noreply.github.com>
2025-05-13 13:19:14 -0600

    added .gitignore

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import sportsRouter from "./routes/sports.js";
+//import sportsRouter from "./routes/sports.js";
 import teamsRouter from "./routes/teams.js";
 import statsRouter from "./routes/stats.js";
 import playersRouter from "./routes/players.js";
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/sports", sportsRouter);
+//app.use("/api/sports", sportsRouter);
 app.use("/api/teams", teamsRouter);
 app.use("/api/stats", statsRouter);
 app.use("/api/players", playersRouter);
