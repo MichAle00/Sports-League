7c8593a8e429ac87a30d5a1457bae552dff2df20
A1rlay <113793274+A1rlay@users.noreply.github.com>
2025-05-20 01:51:15 -0600

    Delete routes/dbInit.js

--- routes/dbInit.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { initDatabase } from '../controllers/dbInitController.js';
-
-const router = Router();
-router.post('/init-db', initDatabase);
-export default router;
